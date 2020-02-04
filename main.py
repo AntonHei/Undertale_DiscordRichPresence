de8e272e981deacaa89b3a9c75fed490c597407d
AntonHei <31041419+AntonHei@users.noreply.github.com>
2020-02-04 16:26:43 +0100

    Implemented a Translation Framerwork, 2 Languages (DE, EN) and a custom Discord Refresh Interval Setting
    Implemented a Translation Framerwork, 2 Languages (German and English) and a custom Discord Refresh Interval Setting (Standard: 10 seconds)

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# ///////////////////////
+# UndertaleDRP by AntonHei
+# MIT License - Copyright (c) 2020 AntonHei
+# ///////////////////////
+
 # Imports
 from pypresence import Presence
 import time
@@ -11,6 +16,8 @@ config = configparser.ConfigParser()
 drpconfig = configparser.ConfigParser()
 undertaleDRPConfig = path.expandvars('config.ini')
 saveGamePath = path.expandvars(r'%LOCALAPPDATA%' + r'\UNDERTALE\undertale.ini')
+undertaleDataPath = path.expandvars(r'data\{language}\undertale_data.json')
+undertaleTranslationPath = path.expandvars(r'data\{language}\main_translations.json')
 
 # UNDERTALE_DRP Data
 config_areaBigLogoState = False
@@ -28,28 +35,36 @@ data_deaths = ""
 
 # Runtime Vars
 startTime = ""
+refreshInterval = 10
 RPC = None
-
-# Others
-areaPicNames = {
-    "Ruinen": "ruins",
-    "Snowdin": "snowdin",
-    "Fehler": "error",
-    "Wasserfall": "waterfall",
-    "Hotland": "hotland",
-    "CORE": "core",
-    "Neues Zuhause": "newhome",
-    "Wahres Labor": "truelab",
-    "Geheim": "secret"
-}
-
-# JSON Init
-with open('undertale_data.json') as f:
-    undertale_data = json.load(f)
+undertale_data = None
+undertale_translations = None
+areaPicNames = None
+curDetails = None
+curState = None
 
 # Config Loadup
-def configLoad():
+def drpConfigLoad():
+    global undertaleDataPath, undertaleTranslationPath
     drpconfig.read(undertaleDRPConfig)
+
+    # Important data loads directly after reading
+    language = str(clearString(getDRPConfigData('UndertaleDRP', 'language')).split(".")[0])
+    undertaleDataPath = undertaleDataPath.replace('{language}', language)
+    undertaleTranslationPath = undertaleTranslationPath.replace('{language}', language)
+
+def loadUndertaleDataJSON():
+    global undertale_data, undertale_translations
+
+    #UndertaleDRP Data
+    with open(undertaleDataPath) as f:
+        undertale_data = json.load(f)
+
+    # UndertaleDRP Translations
+    with open(undertaleTranslationPath) as f:
+         undertale_translations = json.load(f)
+
+def configLoad():
     config.read(saveGamePath)
 
 def discordInit():
@@ -64,14 +79,24 @@ def getDRPConfigData(section, dataname):
 def getConfigData(section, dataname):
     return config[section][dataname]
 
+def listToDict(lst):
+    op = {i: lst[i] for i in range(0, len(lst))}
+    return op
+
 def prepareData():
-    global data_kills, data_roomName, data_roomArea, data_Name, data_LV, data_playtimeSeconds, data_playtimeHours, data_deaths, config_areaBigLogoState, config_discordAppClientID
+    global data_kills, data_roomName, data_roomArea, data_Name, data_LV, data_playtimeSeconds, data_playtimeHours, data_deaths, config_areaBigLogoState, config_discordAppClientID, areaPicNames, refreshInterval
 
     # Getting the UndertaleDRP Config settings
     config_areaBigLogoState = str(clearString(getDRPConfigData('UndertaleDRP', 'areaBigLogo')).split(".")[0])
     config_discordAppClientID = str(clearString(getDRPConfigData('UndertaleDRP', 'discordAppClientID')).split(".")[0])
+    refreshInterval = int(clearString(getDRPConfigData('UndertaleDRP', 'refreshInterval')).split(".")[0])
 
     # Getting the Data
+
+    # Discord App Area Pics
+    areaPicNames = listToDict(list(undertale_data['areaDiscordAppPicNames']))
+
+    # Rooms
     curKills = str(clearString(getConfigData('General', 'Kills')).split(".")[0])
     curRoomINT = int(clearString(getConfigData('General', 'Room')).split(".")[0])
     curName = str(clearString(getConfigData('General', 'Name')).split(".")[0])
@@ -94,63 +119,94 @@ def clearString(str):
     return str
 
 def getAreaPicName(areaname):
-    return areaPicNames.get(areaname)
+    return areaPicNames[0][areaname]
+
+def convertToReadable(str):
+    # Location
+    str = str.replace("{room_Name}", data_roomName)
+    str = str.replace("{room_Area}", data_roomArea)
+
+    # Player
+    str = str.replace("{player_LV}", data_LV)
+    str = str.replace("{player_kills}", data_kills)
+    str = str.replace("{player_deaths}", data_deaths)
+    str = str.replace("{player_name}", data_Name)
+
+    # General
+    str = str.replace("{played_time}", data_playtimeHours)
+
+    return str
+
+def loadTranslations():
+    global curDetails, curState
+
+    # Loading Translations
+    curDetails = convertToReadable(undertale_translations['discordUI'][0]['details'])
+    curState = convertToReadable(undertale_translations['discordUI'][0]['state'])
 
 def startUpdatePresence():
     global startTime
     print("Started Rich Presence")
     startTime = time.time()-int(data_playtimeSeconds)
 
+    # Loading Translations
+    loadTranslations()
+
     if(config_areaBigLogoState == "True"):
         RPC.update(
-            details=data_roomName+" in "+data_roomArea,
+            details=curDetails,
             start=time.time()-int(data_playtimeSeconds),
             large_image=getAreaPicName(data_roomArea),
             large_text=data_roomArea,
-            state="LV: "+data_LV+" with "+data_kills+" kills"
+            state=curState
         )
     else:
         RPC.update(
-            details=data_roomName + " in " + data_roomArea,
+            details=curDetails,
             start=time.time() - int(data_playtimeSeconds),
             small_image=getAreaPicName(data_roomArea),
             small_text=data_roomArea,
             large_image="undertale",
             large_text="UNDERTALE",
-            state="LV: " + data_LV + " with " + data_kills + " kills"
+            state=curState
         )
 
 def updatePresence():
     print("Updated Rich Presence")
 
+    # Loading Translations
+    loadTranslations()
+
     if (config_areaBigLogoState == "True"):
         RPC.update(
-            details=data_roomName + " in " + data_roomArea,
+            details=curDetails,
             start=startTime,
             large_image=getAreaPicName(data_roomArea),
             large_text=data_roomArea,
-            state="LV: " + data_LV + " with " + data_kills + " kills"
+            state=curState
         )
     else:
         RPC.update(
-            details=data_roomName + " in " + data_roomArea,
+            details=curDetails,
             start=startTime,
             small_image=getAreaPicName(data_roomArea),
             small_text=data_roomArea,
             large_image="undertale",
             large_text="UNDERTALE",
-            state="LV: " + data_LV + " with " + data_kills + " kills"
+            state=curState
         )
 
 # Startup Discord Rich Presence
+drpConfigLoad()
 configLoad()
+loadUndertaleDataJSON()
 prepareData()
 discordInit()
 startUpdatePresence()
 
 # Update Prodcedure
 while True:
-    time.sleep(10)
+    time.sleep(refreshInterval)
     configLoad()
     prepareData()
     updatePresence()
